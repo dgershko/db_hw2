d52d66d27186097a41e1bc1048a70fa2b183c357
sacha <slomovits@campus.technion.ac.il>
2024-02-27 12:13:04 +0200

    Adding function documentation

--- Solution.py
+++ Solution.py
@@ -75,92 +75,91 @@ def clear_tables():
     # TODO: implement
     pass
 
-
 def drop_tables():
     # TODO: implement
     pass
 
-
+#Add an owner to the database
 def add_owner(owner: Owner) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Get an owner from the database.
 def get_owner(owner_id: int) -> Owner:
     # TODO: implement
     pass
 
-
+#Delete an owner from the database.
 def delete_owner(owner_id: int) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Add an apartment to the database.
 def add_apartment(apartment: Apartment) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Get an apartment from the database.
 def get_apartment(apartment_id: int) -> Apartment:
     # TODO: implement
     pass
 
-
+#Delete an apartment from the database.
 def delete_apartment(apartment_id: int) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Add a customer to the database.
 def add_customer(customer: Customer) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Get a customer from the database.
 def get_customer(customer_id: int) -> Customer:
     # TODO: implement
     pass
 
-
+#Delete a customer from the database.
 def delete_customer(customer_id: int) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Customer made a reservation of apartment from start_date to end_date and paid total_price
 def customer_made_reservation(customer_id: int, apartment_id: int, start_date: date, end_date: date, total_price: float) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Remove a reservation from the database.
 def customer_cancelled_reservation(customer_id: int, apartment_id: int, start_date: date) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Customer reviewed apartment on date review_date and gave it rating stars, with text review_text.
 def customer_reviewed_apartment(customer_id: int, apartment_id: int, review_date: date, rating: int, review_text: str) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Customer decided to update their review of apartment on update_date and changed his rating to new_rating and the review text to new_text
 def customer_updated_review(customer_id: int, apartmetn_id: int, update_date: date, new_rating: int, new_text: str) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Owner owns apartment. An apartment can be owned by at most one owner.
 def owner_owns_apartment(owner_id: int, apartment_id: int) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Owner dropped apartment and does not own it anymore.
 def owner_drops_apartment(owner_id: int, apartment_id: int) -> ReturnValue:
     # TODO: implement
     pass
 
-
+#Get the owner of apartment.
 def get_apartment_owner(apartment_id: int) -> Owner:
     # TODO: implement
     pass
 
-
+#Get a list of all apartments owned by owner.
 def get_owner_apartments(owner_id: int) -> List[Apartment]:
     # TODO: implement
     pass
@@ -168,21 +167,22 @@ def get_owner_apartments(owner_id: int) -> List[Apartment]:
 
 # ---------------------------------- BASIC API: ----------------------------------
 
+#Get the average rating across all reviews of apartment.
 def get_apartment_rating(apartment_id: int) -> float:
     # TODO: implement
     pass
 
-
+#Get the average of averages of ratings from all reviews of apartments owned by owner.
 def get_owner_rating(owner_id: int) -> float:
     # TODO: implement
     pass
 
-
+#Get the customer that made the most reservations.
 def get_top_customer() -> Customer:
     # TODO: implement
     pass
 
-
+#Output: a list of tuples of (owner_name, total_reservation_count) of all owners in the database.
 def reservations_per_owner() -> List[Tuple[str, int]]:
     # TODO: implement
     pass
@@ -190,11 +190,12 @@ def reservations_per_owner() -> List[Tuple[str, int]]:
 
 # ---------------------------------- ADVANCED API: ----------------------------------
 
+#Return all owners that own an apartment in every city there are apartments in.
 def get_all_location_owners() -> List[Owner]:
     # TODO: implement
     pass
 
-
+#Get the apartment that has the best reviews compared to its average nightly price.
 def best_value_for_money() -> Apartment:
     # TODO: implement
     pass
@@ -204,7 +205,15 @@ def profit_per_month(year: int) -> List[Tuple[int, float]]:
     # TODO: implement
     pass
 
-
+""" In this query you will need to approximate what the given customer will rate an apartment they haven’t been in, based on their and other users’ reviews.
+You will use the following method for the approximation:
+For every customer (other than the one you were given) that has reviewed an apartment the
+given customer also reviewed (or multiple apartments), you will calculate the ratio between
+their ratings (or the average of ratios if there is more than one apartment reviewed by both).
+Then, for every customer you calculated a ratio for, the approximated ratio for an apartment
+they reviewed would be the ratio multiplied by the rating they gave the other apartment. If
+you get multiple approximations for the same apartment, return their average.
+Generate an approximation for all apartments where it is possible. """
 def get_apartment_recommendation(customer_id: int) -> List[Tuple[Apartment, float]]:
     # TODO: implement
     pass
